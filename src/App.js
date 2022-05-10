00253443186baaac096cf04ee453ee5b13529bf5
Prabesh <prabesh.bista369@gmail.com>
2022-05-10 13:56:20 +0545

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+     <Header/>
     </div>
   );
 }
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,9 +2,27 @@ import classes from "./Header.module.css";
 
 const Header=()=>{
     return(
-        <div>
-
+        <header className={classes.header}>
+        <div className={classes.h3}>
+            <h3 >NFT STORE</h3>
         </div>
+        <div className={classes.nav}>
+            <ul>
+                <li>HOME</li>
+                <li>MY PROFILE</li>
+                <li>ACTIVITY</li>
+                <li>HOW IT WOKS</li>
+            </ul>
+        </div>
+        <div className={classes.createDiv}>
+            <button className={classes.create}>
+                CREATE
+            </button>
+            <span className={classes.sign}>SIGN IN</span>
+        </div>
+
+
+        </header>
     )
 
 }
