df96b9f8a75a61b6babd2d9ee9150d4d1b008716
Prabesh <prabesh.bista369@gmail.com>
2022-05-10 15:33:10 +0545

    body

--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -1,4 +1,5 @@
 import classes from "./Body.module.css";
+import {AiOutlineHeart} from "react-icons/ai";
 
 
 const Body=(props)=>{
@@ -9,6 +10,17 @@ const Body=(props)=>{
             </div>
             <div className={classes.boxRight}>
                 <img src={props.imgSrc} alt="hello" />
+                <div className={classes.foot}>
+                    <div className={classes.left}>
+                       <h4>Monke</h4>
+                       <p>0.24 eth</p>
+                    </div>
+                    <div className={classes.right}>
+                        <h4>We are here</h4>
+                        <div className={classes.heart}><AiOutlineHeart/><p>25</p></div>
+                    </div>
+
+                </div>
 
             </div>
         </body>
